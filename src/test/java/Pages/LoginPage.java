9e8b56f8f81ec5b5c68b1d406cf4b9c4d6f4a756
Amena Anjum <amenaanjum5030@gmail.com>
2022-08-22 23:24:19 -0400

    code to select branch and enter user name and password

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -1,7 +1,10 @@
 package Pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 //import org.testng.Assert;
 //import org.openqa.selenium.WebDriver;
 //import org.openqa.selenium.chrome.ChromeDriver;
@@ -14,17 +17,31 @@ public class LoginPage {
 	public void openbrowser(){
 		
 		//launch chrome browser
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\sajjad\\Desktop\\SeleniumDriver\\Chrome_driver\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\sajjad\\Desktop\\SeleniumDriver\\Chrome_driver_v104\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 				
 		//maximize the browser
 		driver.manage().window().maximize();
 				
 		//enter URL
-		driver.get("https://www.ebay.com/");
-
+		driver.get("http://primusbank.qedgetech.com/");
 		
+		String title= driver.getTitle();
+		System.out.println(title);
 		
+		WebElement drpdwn= driver.findElement(By.xpath("//select[@name='drlist']"));
+		
+		Select selObject= new Select(drpdwn);
+		selObject.selectByValue("chinna");	
+		
+		WebElement userName= driver.findElement(By.xpath("//input[@id='txtuId']"));
+		userName.sendKeys("admin");
+		
+		WebElement password= driver.findElement(By.xpath("//input[@name='txtPword']"));
+		password.sendKeys("admin");
+		
+		driver.findElement(By.xpath("//input[@id='login']")).click();
 	}
+	
 
 }
